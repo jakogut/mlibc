9d5e7b6ef123aa7dd4c30c4995054c7e6731d125
Joseph Kogut <joseph.kogut@gmail.com>
2016-12-27 00:24:23 -0800

    stdio function stubs

---
+++ src/file.c
@@ -0,0 +1,18 @@
+/* Need FILE defined for these
+FILE *fopen(const char *path, const char *mode)
+{
+	return NULL;
+}
+
+int fclose(FILE *stream)
+{
+	return 0;
+}
+
+int fputs(const char *str, FILE *stream)
+{
+	return 0;
+}
+*/
+
+
