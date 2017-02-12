abfeae4b1aefddbd6c23f568e925d75d8eacff3c
Joseph Kogut <joseph.kogut@gmail.com>
2017-02-12 15:20:19 -0800

    string: add function prototypes

--- include/string.h
+++ include/string.h
@@ -6,6 +6,16 @@
 typedef unsigned long size_t;
 
 size_t strlen(const char *);
+void *memcpy(void *dest, const void *src, size_t n);
 void *memset(void *s, int c, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
+
+char *strcat(char *dest, const char *src);
+char *strchr(const char *str, int c);
+int strcmp(const char *str1, const char *str2);
+char *strcpy(char *dest, const char *src);
+char *strncpy(char *dest, const char *src, size_t n);
+char *strstr(const char *haystack, const char *needle);
+char *strtok(char *str, const char *delim);
 
 #endif
