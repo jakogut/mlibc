ff38959420b87518ae6e683fbf5c97042e919de6
Joseph Kogut <joseph.kogut@gmail.com>
2017-02-12 12:59:15 -0800

    redefine size_t as long

--- include/stddef.h
+++ include/stddef.h
@@ -3,7 +3,7 @@
 
 #define NULL 0
 
-typedef unsigned int size_t;
+typedef unsigned long size_t;
 typedef long ssize_t;
 typedef long intptr_t;
 
--- include/string.h
+++ include/string.h
@@ -1,6 +1,11 @@
 #ifndef _STRING_H_
 #define _STRING_H_ 1
 
+#define NULL 0
+
+typedef unsigned long size_t;
+
+size_t strlen(const char *);
 void *memset(void *s, int c, size_t n);
 
 #endif
