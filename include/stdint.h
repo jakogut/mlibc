98438e8d574dda95a6025f5b7fe9d80f1cb5e96c
Joseph Kogut <joseph.kogut@gmail.com>
2017-02-12 12:59:46 -0800

    stdint: add signed types

--- include/stdint.h
+++ include/stdint.h
@@ -1,9 +1,14 @@
 #ifndef _STDINT_H_
 #define _STDINT_H_
 
-#define uint8_t unsigned char
-#define uint16_t unsigned short
-#define uint32_t unsigned long
-#define uint64_t unsigned long long
+typedef unsigned char	uint8_t;
+typedef unsigned short	uint16_t;
+typedef unsigned long	uint32_t;
+typedef unsigned long long uint64_t;
+
+typedef char		int8_t;
+typedef short		int16_t;
+typedef int		int32_t;
+typedef long long	int64_t;
 
 #endif
