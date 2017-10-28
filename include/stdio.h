2598cf7187fa64caef7c3028b771180ce1ad6d16
Joseph Kogut <joseph.kogut@gmail.com>
2017-10-28 15:58:47 -0700

    stdio.h: add FILE related definitions

--- include/stdio.h
+++ include/stdio.h
@@ -1,6 +1,28 @@
+#ifndef _STDIO_H_
+#define _STDIO_H_
+
+#include "stddef.h"
+
+#define NULL 0
+
+#define EOF -1
+
+#define STDIN_FILENO	0
+#define STDOUT_FILENO	1
+#define STDERR_FILENO	2
+
+#define FOPEN_MAX	255
+#define FILENAME_MAX	255
+
+#define SEEK_CURR	0
+#define SEEK_END	1
+#define SEEK_SET	2
+
+typedef long fpos_t;
+
 typedef struct {
 	int fd;
-	unsigned long pos;
+	fpos_t pos;
 
 	char mode;
 } FILE;
@@ -19,3 +41,19 @@ static const FILE stderr = {
 	.fd = 2,
 	.pos = 0,
 };
+
+int printf(const char *restrict format, ...);
+char *fgets(char *str, int n, FILE *stream);
+
+FILE *fopen(const char *filename, const char *mode);
+int fclose(FILE *stream);
+
+int feof(FILE *stream);
+long int ftell(FILE *stream);
+int fseek(FILE *stream, long int offset, int whence);
+
+int fgetc(FILE *stream);
+
+void rewind(FILE *stream);
+
+#endif
